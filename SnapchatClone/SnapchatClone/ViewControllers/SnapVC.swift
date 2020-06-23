a952e2131ec5238c8f1eb6f3da8fbe504dfe8443
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-23 23:48:04 +0300

    Image Slide Show

--- SnapchatClone/SnapchatClone/ViewControllers/SnapVC.swift
+++ SnapchatClone/SnapchatClone/ViewControllers/SnapVC.swift
@@ -7,18 +7,43 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class SnapVC: UIViewController {
     
     @IBOutlet weak var timeLabel: UILabel!
     var selectedSnap : Snap?
     var selectedTime : Int?
+    var inputArray = [KingfisherSource]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let time = selectedTime{
             timeLabel.text = "Time Left: \(selectedTime)"
         }
+        
+        if let snap = selectedSnap {
+            
+            for imageUrl in snap.imageUrlArray {
+                inputArray.append(KingfisherSource(urlString: imageUrl)!)
+            }
+            
+            let imageSlideShow = ImageSlideshow(frame: CGRect(x: 10, y: 10, width: self.view.frame.width * 0.95, height: self.view.frame.height * 0.9))
+            imageSlideShow.backgroundColor = UIColor.white
+            
+            let pageIndicator = UIPageControl()
+            pageIndicator.currentPageIndicatorTintColor = UIColor.lightGray
+            pageIndicator.pageIndicatorTintColor = UIColor.black
+            imageSlideShow.pageIndicator = pageIndicator
+            
+            imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFit
+            imageSlideShow.setImageInputs(inputArray)
+            self.view.addSubview(imageSlideShow)
+            self.view.bringSubviewToFront(timeLabel)
+            
+            
+        }
+        
     }
     
     
